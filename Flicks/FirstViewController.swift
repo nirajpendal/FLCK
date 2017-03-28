14dac55f9362d0dfa78dc49c36c9624776882912
npendal <npendal@proteus.com>
2017-03-27 21:43:44 -0700

    Initial Commit

---
+++ Flicks/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Flicks
+//
+//  Created by Niraj Pendal on 3/27/17.
+//  Copyright © 2017 Proteus. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
