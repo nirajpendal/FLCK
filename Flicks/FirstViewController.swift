331f502b6e7ada0f67e0bdd8052a59dd4e5a474f
npendal <npendal@proteus.com>
2017-03-31 21:37:46 -0700

    Refactoring

--- Flicks/FirstViewController.swift
+++ Flicks/FirstViewController.swift
@@ -14,29 +14,7 @@ class FirstViewController: MovieDisplayViewController {
         self.presentIndicator()
         movieHelper.getNowPlayingMovies { [weak self] (moviesFromResponse, error) in
             
-            guard let strongSelf = self else {
-                return
-            }
-            
-            if error != nil {
-                // Present error dialog here..
-                print(error!.localizedDescription)
-                let alertView = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                
-                let okActionButton: UIAlertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-                alertView.addAction(okActionButton)
-                
-                strongSelf.present(alertView, animated: true, completion: nil)
-                
-            } else {
-                print("Movies returned \(moviesFromResponse.count)")
-                strongSelf.movies = moviesFromResponse
-                strongSelf.movieCopy = strongSelf.movies
-                strongSelf.moviesTableView.reloadData()
-            }
-            
-            strongSelf.hideIndicator()
-            strongSelf.refreshControl.endRefreshing()
+            self?.resultReturned(movieResults: moviesFromResponse, error: error)
         }
     }
     
--- Flicks/MovieDisplayViewController.swift
+++ Flicks/MovieDisplayViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AFNetworking
 
-class MovieDisplayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+class MovieDisplayViewController:  UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     @IBOutlet weak var moviesTableView: UITableView!
     var movieHelper = Movies()
@@ -57,9 +57,30 @@ class MovieDisplayViewController: UIViewController, UITableViewDelegate, UITable
         self.activityIndicator.hideActivityIndicator(view: (self.tabBarController?.view)!)
     }
     
-    
     func getMoviesAndUpdateTable() {
-       // self.presentIndicator()
+       // Will be impleteemented by sub-classes
+    }
+    
+    func resultReturned(movieResults:[Movie], error:Error?)  {
+        if error != nil {
+            // Present error dialog here..
+            print(error!.localizedDescription)
+            let alertView = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+            
+            let okActionButton: UIAlertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertView.addAction(okActionButton)
+            
+            self.present(alertView, animated: true, completion: nil)
+            
+        } else {
+            print("Movies returned \(movieResults.count)")
+            self.movies = movieResults
+            self.movieCopy = self.movies
+            self.moviesTableView.reloadData()
+        }
+        
+        self.hideIndicator()
+        self.refreshControl.endRefreshing()
     }
     
     
